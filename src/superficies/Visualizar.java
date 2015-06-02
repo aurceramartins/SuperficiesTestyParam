4fed27527b83692dcbb47986b3ff11ea362f334c
Alex Urcera <Alex Urcera@Alex>
2015-06-02 10:16:30 +0200

    Commit

--- src/superficies/Visualizar.java
+++ src/superficies/Visualizar.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package superficies;
 
@@ -10,16 +5,8 @@ import java.util.Scanner;
 import javax.swing.JOptionPane;
 import static superficies.logic.calcular;
 
-/**
- * commit añadido tambien
- * @author mpereirasalgado
- */
 public class Visualizar {
 
-    /**
-     * metodo que nos manda en ventana el resultado del area calculada
-     * @param contenedor
-     */
     
     public static void menu(Modelo contenedor){
         int opcion = 0;
